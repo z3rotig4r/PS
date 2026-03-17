06ce4b2a1f6424c2fd9c54f348c40729d0ee8d27
Youngbeom Cho <zerotiger4764@gmail.com>
2026-03-17 12:01:06 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, k;
+    cin >> n >> k;
+
+    queue<int> q;
+    for(int i=1; i<=n; i++) q.push(i);
+    cout << '<';
+    while (!q.empty()) {
+        for(int i=0; i<k-1; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+        cout << q.front();
+        q.pop();
+
+        if(!q.empty()) {
+            cout << ", ";
+        }
+    }
+    cout << ">\n";
+    return 0;
+}
