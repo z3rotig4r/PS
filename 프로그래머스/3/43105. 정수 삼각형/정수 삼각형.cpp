367e6d8a929445efd66054f9fe6279399f26c30a
Youngbeom Cho <zerotiger4764@gmail.com>
2026-03-26 14:37:42 +0900

    [level 3] Title: 정수 삼각형, Time: 0.35 ms, Memory: 7.83 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> triangle) {
+    int answer = 0;
+    for(int i=triangle.size()-2; i>=0; i--){
+        for(int j=0; j<triangle[i].size(); j++){
+            triangle[i][j] += max(triangle[i+1][j], triangle[i+1][j+1]);
+        }
+    }
+    answer = triangle[0][0];
+    
+    return answer;
+}
